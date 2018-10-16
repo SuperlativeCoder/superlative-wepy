d8761ea2c85c0ecd8f61b8d15a6b32c174130c35
ChangerHe <244737162@qq.com>
2018-10-16 10:02:59 +0800

    feat: add redux logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,15 @@
 import { createStore, applyMiddleware } from 'redux'
 import promiseMiddleware from 'redux-promise'
 import rootReducer from './reducers'
+import { createLogger } from 'redux-logger'
+
+const middlewares = [promiseMiddleware]
+middlewares.push(createLogger({
+  collapsed: true,
+  diff: true
+}))
 
 export default function configStore () {
-  const store = createStore(rootReducer, applyMiddleware(promiseMiddleware))
+  const store = createStore(rootReducer, applyMiddleware(...middlewares))
   return store
-}
+};
