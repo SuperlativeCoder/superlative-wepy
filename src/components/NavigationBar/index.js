6042f403dbd40818fa121063a88665e0ea496a44
ChangerHe <244737162@qq.com>
2018-10-30 17:30:17 +0800

    feat: add navigate back demo

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -42,6 +42,18 @@ export default class NavigationNavBar extends wepy.component {
       type: Boolean,
       default: true,
     },
+    navigateBackType: {
+      type: Number,
+      default: 1,
+    },
+    navigateBackModalTitle: {
+      type: String,
+      default: '',
+    },
+    navigateBackModalContent: {
+      type: String,
+      default: '确定要返回吗?',
+    },
   }
   data = {
     height: isIOS ? IOS_NAVBAR_HEIGHT : ANDROID_NAVHAR_HEIGHT,
@@ -66,14 +78,24 @@ export default class NavigationNavBar extends wepy.component {
   methods = {
     handleNavigate(param) {
       const that = this;
-      if (!param) {
+      if (!param.navigateBackType && this.navigateBackType === 1) {
         this.methods.navigateBack();
-      } else if (param && param.type === 1) {
+      } else if ((param && param.navigateBackType === 2) || this.navigateBackType === 2) {
         this.methods.navigateBackHome();
-      } else if (param && param.type === 2 && param.content) {
+      } else if (param && param.navigateBackType === 3 && param.content) {
         wx.showModal({
           title: param.title || '',
           content: param.content,
+          success(res) {
+            if (res.confirm) {
+              that.methods.navigateBack();
+            }
+          },
+        });
+      } else if (this.navigateBackType === 3 && this.navigateBackModalContent) {
+        wx.showModal({
+          title: this.navigateBackModalTitle,
+          content: this.navigateBackModalContent,
           success(res) {
             if (res.confirm) {
               that.methods.navigateBack();
